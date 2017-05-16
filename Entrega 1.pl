142ced7f026e25f0bbc1034a21851822af84e0ff
Quimey Lucas Marquez <qmarquez@est.frba.utn.edu.ar>
2017-05-16 00:45:19 -0300

    Creacion alquiler los simpsons II.

--- Entrega 1.pl
+++ Entrega 1.pl
@@ -1,3 +1,4 @@
 alquiler("Tinsmith Circle 1774",700,["3 ambientes","Jardin"]).
 alquiler("Av. Moreno 708",2000,["7 ambientes","Jardin","Pileta 30m3"]).
 alquiler("Av. Siempre Viva 742",1000,["4 ambientes","Jardin"]).
+alquiler("Calle falsa 123",200,["3 ambientes","Sin jardin"]).
