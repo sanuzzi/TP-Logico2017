b31f6bc472579be3c4f1d105cf651c02b09d257a
Quimey Lucas Marquez <qmarquez@est.frba.utn.edu.ar>
2017-05-17 16:26:12 -0300

    Reestructuracion de predicados.

--- Entrega 1.pl
+++ Entrega 1.pl
@@ -1,4 +1,15 @@
-alquiler(tinsmithCircle1774,700,[ambientes3,jardin]).
-alquiler(avMoreno708,2000,[ambientes7,jardin,pileta30m3]).
-alquiler(avSiempreViva742,1000,[ambientes7,jardin]).
-alquiler(calleFalsa123,200,[ambientes3,sinjardin]).
+alquiler(tinsmithCircle1774, 700).
+alquiler(avMoreno708, 2000).
+alquiler(avSiempreViva742,1000).
+alquiler(calleFalsa123, 200).
+
+ambientes(tinsmithCircle1774,3).
+ambientes(avMoreno708,7).
+ambientes(avSiempreViva742,4).
+ambientes(calleFalsa123,3).
+
+jardin(tinsmithCircle1774).
+jardin(avMoreno708).
+jardin(avSiempreViva742).
+
+piscina(avMoreno708,30).
