eaae1e6f156c4f7b3d340093032244ab0fd499a4
Quimey Lucas Marquez <qmarquez@est.frba.utn.edu.ar>
2017-05-17 16:33:23 -0300

    Primera parte de consultas.

--- Entrega 1.pl
+++ Entrega 1.pl
@@ -13,3 +13,23 @@ jardin(avMoreno708).
 jardin(avSiempreViva742).
 
 piscina(avMoreno708,30).
+
+% 1.
+% ?- piscina(X,30).
+% X = avMoreno708.
+
+igualAmbientes(Casa1,Casa2):-
+  ambientes(Casa1,UnosAmbientes),
+  ambientes(Casa2,UnosAmbientes).
+
+% 2.
+% ?-
+% ?- igualAmbientes(X,Y).
+% X = Y, Y = tinsmithCircle1774 ;
+% X = Y, Y = avMoreno708 ;
+% X = Y, Y = avSiempreViva742 ;
+% X = Y, Y = calleFalsa123.
+% X = tinsmithCircle1774,
+% Y = calleFalsa123 ;
+% X = calleFalsa123,
+% Y = tinsmithCircle1774 ;
